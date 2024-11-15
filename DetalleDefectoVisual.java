e7fc3be985fcc3cd51a23094b3fc9195ffe0501c
melpederneraa <167656054+melpederneraa@users.noreply.github.com>
2024-11-15 18:00:42 -0300

    Create DetalleDefectoVisual.java

---
+++ DetalleDefectoVisual.java
@@ -0,0 +1,17 @@
+package paquete;
+
+public class DetalleDefectoVisual {
+    private String descripcion;
+
+    public DetalleDefectoVisual(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
